36850a24d4a53844b3b3332b0b9c6bd0d6b05bf5
tosinakinro <116086170+tosinakinro@users.noreply.github.com>
2024-06-07 13:42:06 -0500

    Updating vpc outputs

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -5,3 +5,19 @@ output "vpc_id" {
 output "subnet_ids" {
   value = aws_subnet.private_subnet[*].id
 }
+
+output "security_group_ingress_port" {
+  value = module.security-group.security_group_ingress_port
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "db_availability_zone" {
+  value = module.rds-instance.db_availability_zone
+}
+
+output "db_instance_id" {
+  value = module.rds-instance.db_instance_id
+}
