c1b4b2e5c3fe60a87bc7d30243495700700df419
tosinakinro <116086170+tosinakinro@users.noreply.github.com>
2024-06-07 13:41:24 -0500

    Updating SG outputs

--- security-group/outputs.tf
+++ security-group/outputs.tf
@@ -1,3 +1,7 @@
 output "security_group_id" {
   value = aws_security_group.rds_sg.id
 }
+
+output "security_group_ingress_port" {
+  value = element([for rule in aws_security_group.rds_sg.ingress : rule.to_port], 0)
+}
